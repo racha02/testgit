69432c4cbbc5648de1bbfcd7c3d31414432e1202
malai2543 <malaisui095@gmail.com>
2021-03-18 16:18:22 +0700

    add f3

--- Hello.py
+++ Hello.py
@@ -1,7 +1,14 @@
 def hello():
     for i in range(10):
         print("hello:",i+1)
+
 def f1():
     print("F1")
+
+def f2():
+    print("F2")
+
+def f3():
+    print("F3")
 name = input ("Enter your name:")
 print(f"hello {name}")
